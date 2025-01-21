7980f0227b9cbaae9761aa10933447beaf6278ad
양종원 <yjw963@gmail.com>
2025-01-21 13:43:22 +0900

    BOJ2587 solved

---
+++ src/x01/BOJ2587.java
@@ -0,0 +1,23 @@
+package x01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ2587 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        int[] arr = new int[5];
+        for (int i = 0; i < 5; i++) {
+            int num = Integer.parseInt(br.readLine());
+            sum += num;
+            arr[i] = num;
+        }
+        Arrays.sort(arr);
+        System.out.println(sum / 5);
+        System.out.println(arr[2]);
+    }
+}
