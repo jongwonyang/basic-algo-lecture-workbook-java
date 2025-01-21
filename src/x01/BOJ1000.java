f0c5401632855217c8c05b8443f6510c0d50587a
양종원 <yjw963@gmail.com>
2025-01-21 12:49:53 +0900

    BOJ1000 solved

---
+++ src/x01/BOJ1000.java
@@ -0,0 +1,14 @@
+package x01;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a + b);
+    }
+}
