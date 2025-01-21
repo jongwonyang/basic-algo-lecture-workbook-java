4d66a833c5e84ac687db30f4c9d6e513cd955251
양종원 <yjw963@gmail.com>
2025-01-21 14:36:54 +0900

    BOJ10804 solved

---
+++ src/x01/BOJ10804.java
@@ -0,0 +1,32 @@
+package x01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ10804 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[21];
+        for (int i = 1; i <= 20; i++) arr[i] = i;
+
+        for (int i = 0; i < 10; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            while (a < b) {
+                int tmp = arr[a];
+                arr[a] = arr[b];
+                arr[b] = tmp;
+                a++;
+                b--;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 20; i++) sb.append(arr[i]).append(' ');
+        System.out.println(sb);
+    }
+}
