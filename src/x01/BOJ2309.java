377b404229cfd8bc08850b1143c95583c12320a0
양종원 <yjw963@gmail.com>
2025-01-21 13:49:45 +0900

    BOJ2309 solved

---
+++ src/x01/BOJ2309.java
@@ -0,0 +1,32 @@
+package x01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ2309 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    for (int k = 0; k < 9; k++) {
+                        if (k != i && k != j) System.out.println(arr[k]);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
