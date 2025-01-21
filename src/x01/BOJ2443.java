3d299d97c83c62c55c346483956b0db9479c6b04
jongwonyang <yjw963@gmail.com>
2025-01-21 17:11:37 +0900

    BOJ2443 solved

---
+++ src/x01/BOJ2443.java
@@ -0,0 +1,17 @@
+package x01;
+
+import java.util.Scanner;
+
+public class BOJ2443 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) sb.append(" ");
+            for (int j = 0; j < N + N - (2 * (i + 1) - 1); j++) sb.append("*");
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
