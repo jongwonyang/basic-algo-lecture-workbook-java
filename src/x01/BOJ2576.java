9e0dd6f678120cd7a2f3cd46486ebb6cf6333563
양종원 <yjw963@gmail.com>
2025-01-21 13:40:22 +0900

    BOJ2576 solved

---
+++ src/x01/BOJ2576.java
@@ -0,0 +1,24 @@
+package x01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2576 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0, min = Integer.MAX_VALUE;
+        for (int i = 0; i < 7; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num % 2 != 0) {
+                sum += num;
+                min = Math.min(min, num);
+            }
+        }
+        if (sum == 0) System.out.println(-1);
+        else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+}
