0b2b89eaadbde72e09f75ffe557aeff8d6391ce3
jongwonyang <yjw963@gmail.com>
2025-01-21 16:43:42 +0900

    BOJ2438 solved

---
+++ src/x01/BOJ2438.java
@@ -0,0 +1,16 @@
+package x01;
+
+import java.util.Scanner;
+
+public class BOJ2438 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < i; j++) sb.append('*');
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
