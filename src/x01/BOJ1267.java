19872d3e57e3ab9a153898bd6d82163dba81f644
양종원 <yjw963@gmail.com>
2025-01-21 14:06:19 +0900

    BOJ1267 solved

---
+++ src/x01/BOJ1267.java
@@ -0,0 +1,27 @@
+package x01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1267 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int y = 0, m = 0;
+        for (int i = 0; i < N; i++) {
+            int sec = Integer.parseInt(st.nextToken());
+            y += 10 + (sec / 30) * 10;
+            m += 15 + (sec / 60) * 15;
+        }
+        if (y < m) {
+            System.out.println("Y " + y);
+        } else if (y > m) {
+            System.out.println("M " + m);
+        } else {
+            System.out.println("Y M " + y);
+        }
+    }
+}
