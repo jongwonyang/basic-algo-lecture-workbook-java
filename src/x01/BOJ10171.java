6456a2ef045b8f4c05191d3e939d5fafdc58d9da
양종원 <yjw963@gmail.com>
2025-01-21 13:03:29 +0900

    BOJ10171 solved

---
+++ src/x01/BOJ10171.java
@@ -0,0 +1,10 @@
+package x01;
+
+public class BOJ10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
