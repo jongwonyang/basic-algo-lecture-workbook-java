9b25fe70d69082433f068f401f767983c2d93847
jongwonyang <yjw963@gmail.com>
2025-01-21 17:33:17 +0900

    BOJ2562 solved

---
+++ src/x01/BOJ2562.java
@@ -0,0 +1,21 @@
+package x01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int maxVal = 0, maxIdx = -1;
+        for (int i = 0; i < 9; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num > maxVal) {
+                maxVal = num;
+                maxIdx = i;
+            }
+        }
+        System.out.println(maxVal);
+        System.out.println(maxIdx + 1);
+    }
+}
