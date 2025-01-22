3364c38b27a5337a7d82a4af30e60a4aa782d311
양종원 <yjw963@gmail.com>
2025-01-22 19:33:23 +0900

    BOJ10773 solved

---
+++ src/x05/BOJ10773.java
@@ -0,0 +1,27 @@
+package x05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class BOJ10773 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int K = Integer.parseInt(br.readLine());
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < K; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) stack.pop();
+            else stack.push(num);
+        }
+
+        int sum = 0;
+        while (!stack.isEmpty()) sum += stack.pop();
+
+        System.out.println(sum);
+    }
+}
