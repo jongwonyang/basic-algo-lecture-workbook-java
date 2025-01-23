2ee8662302e38beddd1230feb4f9a604520dc717
양종원 <yjw963@gmail.com>
2025-01-23 13:36:16 +0900

    BOJ10845 solved

---
+++ src/x06/BOJ10845.java
@@ -0,0 +1,47 @@
+package x06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class BOJ10845 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> q = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String op = st.nextToken();
+            switch (op) {
+                case "push":
+                    int x = Integer.parseInt(st.nextToken());
+                    q.offer(x);
+                    break;
+                case "pop":
+                    if (q.isEmpty()) sb.append(-1).append("\n");
+                    else sb.append(q.poll()).append("\n");
+                    break;
+                case "size":
+                    sb.append(q.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(q.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    if (q.isEmpty()) sb.append(-1).append("\n");
+                    else sb.append(q.peekFirst()).append("\n");
+                    break;
+                case "back":
+                    if (q.isEmpty()) sb.append(-1).append("\n");
+                    else sb.append(q.peekLast()).append("\n");
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
