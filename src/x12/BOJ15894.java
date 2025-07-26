61ab12c828e010d3236ac0cb90b497ad069fb090
jongwonyang <yjw963@gmail.com>
2025-07-26 19:50:06 +0900

    BOJ15894 solved

---
+++ src/x12/BOJ15894.java
@@ -0,0 +1,14 @@
+package x12;
+
+import java.util.Scanner;
+
+public class BOJ15894 {
+
+    static int N;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        System.out.println(4 * (long) N);
+    }
+}
